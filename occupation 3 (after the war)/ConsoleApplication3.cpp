9fcc85328c5a654b61dbb18ae4354c47e205768e
bogdanverb <103574969+bogdanverb@users.noreply.github.com>
2022-05-25 17:23:51 +0300

    Add files via upload

---
+++ occupation 3 (after the war)/ConsoleApplication3.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cmath>
+#include <conio.h>
+#include <cstdlib>
+#include <string>
+#include <ctime>
+using namespace std;
+
+int x, y;
+
+void dobuleNumber() {
+	cout << "Result - ";
+	cout << x * y;
+}
+
+int main() {
+	cout << "First number\n";
+	cin >> x;
+	cout << "Second number\n";
+	cin >> y;
+	dobuleNumber();
+}
+
